3e943f7b05d49c8f595bf4c35256e69bff868a76
Dan DeLeo <danielsdeleo@mac.com>
2009-07-05 15:30:55 -0600

    make Math extensions rdoc'ed and more pleasing to the eye

--- lib/decider/core_extensions.rb
+++ lib/decider/core_extensions.rb
@@ -2,24 +2,22 @@
 
 module Math
   
+  # Variance of a population. If computing the variance of the entire 
+  # population, defaults are fine. If computing the variance of a sample of the
+  # population, enable the "Bessel Correction" with <tt>:sample => true</tt>
   def variance(population, opts={:sample=>false})
-    n = 0
-    mean = 0.0
-    s = 0.0
-    population.each { |x|
-      n = n + 1
+    n, mean, s = 0, 0.0, 0.0
+    population.each do |x|
+      n += 1
       delta = x - mean
-      mean = mean + (delta / n)
-      s = s + delta * (x - mean)
-    }
-    # if you want to calculate std deviation
-    # of a sample change this to "s / (n-1)"
+      mean += (delta / n)
+      s += delta * (x - mean)
+    end
     return opts[:sample] ? s / (n - 1.0) : s / n
   end
   
-  # calculate the standard deviation of a population
-  # accepts: an array, the population
-  # returns: the standard deviation
+  # Standard Deviation of a population. For a sample of a population, you need
+  # to provide <tt>:sample => true</tt> (See #variance for more)
   def stddev(population, opts={:sample=>false})
     sqrt(variance(population, opts))
   end
