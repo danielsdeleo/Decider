a9008a28c971441cd313caeac77628990bf09a9d
Dan DeLeo <danielsdeleo@mac.com>
2009-07-06 22:01:22 -0600

    token selector is obsoleted. boom.

--- lib/decider.rb
+++ lib/decider.rb
@@ -6,7 +6,6 @@ unless defined?(DECIDER_ROOT)
   DECIDER_ROOT = File.dirname(__FILE__) + '/'
 end
 
-require DECIDER_ROOT + "/decider/token_selector"
 require DECIDER_ROOT + "/decider/training_set"
 require DECIDER_ROOT + "decider/token"
 require DECIDER_ROOT + "decider/core_extensions"
