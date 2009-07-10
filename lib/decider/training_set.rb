e0d67b276223a8006cdec3c3a6076d4094dcedff
Dan DeLeo <danielsdeleo@mac.com>
2009-07-09 23:25:22 -0600

    factor document analysis out of training set. it don't belong there

--- lib/decider/training_set.rb
+++ lib/decider/training_set.rb
@@ -59,78 +59,12 @@ module Decider
       term_frequency
     end
     
-    def index_of(token)
-      if token = @tokens[token]
-        token.index
-      end
-    end
-    
     def count_of(token)
       @tokens[token].count
     end
     
-    def vectorize(tokens)
-      vector = Array.new(@tokens.length, 0)
-      tokens.each do |token| 
-        if index = index_of(token)
-          vector[index] = 1
-        end
-      end
-      vector
-    end
-    
-    def probability_of_token(token)
-      if token = @tokens[token]
-        token.count.to_f / total_tokens.to_f
-      else
-        hapax_occurrence_value / total_tokens.to_f
-      end
-    end
-    
-    def probability_of_tokens(tokens)
-      (tokens.inject(0) { |sum, token| sum + probability_of_token(token) }) / tokens.count.to_f
-    end
-    
-    # Gives the probability that the document given by +document_string+ belongs
-    # to this set (document class)
-    def probability_of_document(document_string)
-      probability_of_tokens new_document(document_string).tokens
-    end
-    
-    def total_tokens
-      @token_count ||= @tokens.inject(0) { |sum, key_val| sum + key_val.last.count }
-    end
-    
-    def probabilities_of_documents
-      @documents.map { |d| d.probability }
-    end
-    
-    def avg_document_probability
-      @avg_document_probability ||= probabilities_of_documents.inject(0) { |sum, prob| sum + prob } / @documents.count.to_f
-    end
-    
-    def document_score_stddev
-      @document_score_stddev ||= Math.stddev(probabilities_of_documents)
-    end
-    
-    def distance_from_avg_in_stddevs(tokens)
-      (probability_of_tokens(tokens) - avg_document_probability) / document_score_stddev
-    end
-    
-    def anomaly_score_of(tokens)
-      @anomaly_score_of_tokens ||= {}
-      if result = @anomaly_score_of_tokens[tokens]
-        result
-      else
-        number_std_devs = distance_from_avg_in_stddevs(tokens)
-        @anomaly_score_of_tokens[tokens] = number_std_devs > 0 ? 0 : -1.0 * number_std_devs
-      end
-    end
-    
-    # Gives the number of Standard Deviations that +document_string+ is, 
-    # probability-wise, from the average. Experimental, YMMV.
-    def anomaly_score_of_document(document_string)
-      anomaly_score_of(new_document(document_string).tokens)
+    def doc_count
+      @documents.count
     end
     
     private
--- spec/unit/training_set_spec.rb
+++ spec/unit/training_set_spec.rb
@@ -48,32 +48,32 @@ describe TrainingSet do
       @training_set.count_of("cheese").should == 2
     end
   
-    it "should find the vector (array) index for a token" do
-      @training_set << "parameter" << "other_parameter"
-      @training_set.index_of("other_parameter").should == 1
-    end
+    #it "should find the vector (array) index for a token" do
+    #  @training_set << "parameter" << "other_parameter"
+    #  @training_set.index_of("other_parameter").should == 1
+    #end
+    #
+    #it "should not fail when trying to find the vector index for a token not in the set" do
+    #  @training_set << "a_parm"
+    #  lambda {@training_set.index_of("unknown_token")}.should_not raise_error
+    #end
+    #
+    #it "should generate a vector representation of a list of tokens" do
+    #  @training_set << "parameter" << "other_parm" << "exploitable_php_app.php"
+    #  @training_set.vectorize(["parameter", "other_parm"]).should == [1,1,0]
+    #end
+    #
+    #it "should not fail when generating a vector for a term with unknown tokens" do
+    #  @training_set << "parameter" << "other_parm" << "exploitable_php_app.php"
+    #  @training_set.vectorize(["unknown_tokens"]).should == [0,0,0]
+    #end
   
-    it "should not fail when trying to find the vector index for a token not in the set" do
-      @training_set << "a_parm"
-      lambda {@training_set.index_of("unknown_token")}.should_not raise_error
-    end
-  
-    it "should generate a vector representation of a list of tokens" do
-      @training_set << "parameter" << "other_parm" << "exploitable_php_app.php"
-      @training_set.vectorize(["parameter", "other_parm"]).should == [1,1,0]
-    end
-  
-    it "should not fail when generating a vector for a term with unknown tokens" do
-      @training_set << "parameter" << "other_parm" << "exploitable_php_app.php"
-      @training_set.vectorize(["unknown_tokens"]).should == [0,0,0]
-    end
-  
-    it "should compute the total number of tokens and cache it" do
-      @training_set << "cheese" << "dumpster" << "oldskool"
-      @training_set.total_tokens.should == 3
-      @training_set << "cheese" << "dumpster" << "oldskool"
-      @training_set.total_tokens.should == 6
-    end
+    #it "should compute the total number of tokens and cache it" do
+    #  @training_set << "cheese" << "dumpster" << "oldskool"
+    #  @training_set.total_tokens.should == 3
+    #  @training_set << "cheese" << "dumpster" << "oldskool"
+    #  @training_set.total_tokens.should == 6
+    #end
   end
   
   context "with training data loaded" do
@@ -115,65 +115,8 @@ describe TrainingSet do
       @training_set.term_frequency.should == expected
     end
     
-    it "should give the probability of a token in the set" do
-      @training_set.probability_of_token("bullet.gif").should == (2.0 / 20.0)
-    end
-    
-    it "should give the probability of a set of tokens" do
-      prob = @training_set.probability_of_tokens(["bullet.gif", "bullet2.gif", "technical"])
-      prob.should == ((2.0/ 20.0) + (2.0/ 20.0) + (1.0 / 20.0)) / 3.0
-    end
-    
-    it "should give the probability of a set of tokens that includes an unknown token" do
-      prob = @training_set.probability_of_tokens(["bullet.gif", "bullet2.gif", "unknown_token"])
-      prob.should == ((2.0/ 20.0) + (2.0/ 20.0) + (0.0 / 20.0)) / 3.0
-    end
-    
-    it "should give the probabilty of a document" do
-      prob = @training_set.probability_of_document("bullet.gif bullet2.gif unknownToken")
-      prob.should == ((2.0/ 20.0) + (2.0/ 20.0) + (0.0 / 20.0)) / 3.0
-    end
-    
-    it "should compute the probabilities of all documents" do
-      expected = TOKENS.map { |t| @training_set.probability_of_token(t) }.sort
-      @training_set.probabilities_of_documents.sort.should == expected
-    end
-    
-    it "should give the average probability of the documents" do
-      expected_probs = TOKENS.map { |t| @training_set.probability_of_token(t) }
-      expected_avg = expected_probs.inject(0) { |sum, prob| sum + prob } / TOKENS.count.to_f
-      @training_set.avg_document_probability.should == expected_avg
-    end
-    
-    it "should give the standard deviation in document probabilities" do
-      @training_set.document_score_stddev.should be_close(0.0312, 0.0001)
-    end
-    
-    it "should give the number of standard deviations a document is from the mean" do
-      expected = 0.005 / @training_set.document_score_stddev
-      @training_set.distance_from_avg_in_stddevs(["bullet.gif"]).should be_close(expected, 0.00000001)
-    end
-    
-    it "should give the stddev distance for an unknown token" do
-      expected = -1 * @training_set.avg_document_probability / @training_set.document_score_stddev
-      @training_set.distance_from_avg_in_stddevs(["unknown_token"]).should be_close(expected, 0.00000001)
-    end
-    
-    it "should compute an ``anomaly score'' == {0 if prob - avg > 0; #of std devs if prob - avg < 0} " do
-      expected = @training_set.avg_document_probability / @training_set.document_score_stddev
-      @training_set.anomaly_score_of(["unknown_token"]).should be_close(expected, 0.00000001)
-      @training_set.anomaly_score_of(["bullet.gif"]).should == 0.0
-    end
-    
-    it "should create a document from text and compute its ``anomaly_score''" do
-      @training_set.should_receive(:anomaly_score_of).with(%w{foo bar baz}).and_return(0.23)
-      @training_set.anomaly_score_of_document("foo bar baz").should == 0.23
-    end
-    
-    it "should memoize the results of #anomaly_score_of(tokens)" do
-      @training_set.should_receive(:distance_from_avg_in_stddevs).once.and_return(3.0)
-      @training_set.anomaly_score_of(["unknown_token"])
-      @training_set.anomaly_score_of(["unknown_token"])
+    it "should give the document count" do
+      @training_set.doc_count.should == 20
     end
     
   end
