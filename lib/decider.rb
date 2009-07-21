796211405514daa5e1996b85ba7c8a258cfff1de
Dan DeLeo <danielsdeleo@mac.com>
2009-07-21 00:09:48 -0600

    rm'd cache invalidation -- easier to do w/ inheritance

--- lib/decider.rb
+++ lib/decider.rb
@@ -7,7 +7,6 @@ unless defined?(DECIDER_ROOT)
   DECIDER_ROOT = File.dirname(__FILE__) + '/'
 end
 
-require DECIDER_ROOT + "decider/cache_invalidation"
 require DECIDER_ROOT + "decider/token"
 require DECIDER_ROOT + "decider/core_extensions"
 require DECIDER_ROOT + "decider/stopwords"
