3d9e2089d1976c49e9466237b80b4e29862b6c45
Dan DeLeo <danielsdeleo@mac.com>
2009-08-03 19:50:31 -0600

    rm pending spec that had already been addressed

--- spec/unit/clustering/node_spec.rb
+++ spec/unit/clustering/node_spec.rb
@@ -63,10 +63,6 @@ describe Clustering::Node do
       @test_low = C.new(:test_low, [0,0,1,1])
     end
     
-    it "should support arbitrary vectoring methods" do
-      pending
-    end
-  
     it "should find the closest vector " do
       @root_node.index_of_child_closest_to(@test_high).should == 0
       @root_node.index_of_child_closest_to(@test_low).should == 1
