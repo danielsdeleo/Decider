8da34aa425ccf34dd08027cdc1a178d6a9711365
Dan DeLeo <danielsdeleo@mac.com>
2009-08-07 07:58:30 -0600

    a bit more progress towards actually getting the results

--- benchmark/nearest_neighbors_clusters_bench.rb
+++ benchmark/nearest_neighbors_clusters_bench.rb
@@ -15,7 +15,7 @@ module NearestNeighborsClustersBench
       @users_repos[user] << repo
     end
 
-    p users_repos.size
+    stats.total_users = users_repos.size
 
     @users_repos.each do |user, repos|
       clusterer.push(user, repos)
@@ -29,10 +29,41 @@ module NearestNeighborsClustersBench
     end
     users
   end
+  
+  def stats
+    @stats ||= GithubStats.new
+  end
+  
+  class GithubStats
+    attr_accessor :total_users
+  end
+  
+  class Recommendation
+    def initialize(user_id)
+      @user_id = user_id
+    end
+    
+    def similar_users=(similar_users_vectors)
+      @similar_users = similar_users_vectors.map { |user| user.doc.name }
+    end
+    
+    def ranked_repos(users_repos)
+      already_watching = users_repos[@user_id]
+      watched_by_similar_users = Hash.new {0}
+      @similar_users.each do |user|
+        users_repos[user].each do |repo|
+          watched_by_similar_users[repo] += 1 unless already_watching.include?(repo)
+        end
+      end
+      watched_by_similar_users
+    end
+    
+  end
 end
 
 cluster = Decider::Clustering::NearestNeighbors.new { |doc| doc.verbatim }
 NNCB = NearestNeighborsClustersBench
+recommendations = []
 
 Benchmark.bm(20) do |results|
   results.report("load data:") do
@@ -43,10 +74,27 @@ Benchmark.bm(20) do |results|
     cluster.tree
   end
   
-  results.report(":find KNNs") do
-    NNCB.users_to_recommend_to.each do |userid|
+  results.report("find KNNs:") do
+    # users_to_recommend_to = NNCB.users_to_recommend_to
+    # Use threads here: 
+    # users1 = users_to_recommend_to.slice!(Range.new(0, users_to_recommend_to.size / 2 - 1))
+    # users2 = users_to_recommend_to
+    recommendations = NNCB.users_to_recommend_to.map do |userid|
       vector = NNCB.users_repos[userid]
-      p cluster.knn(10, vector)
+      knn =  cluster.knn(10, vector)
+      puts "for user #{userid}, found these similar users:"
+      puts knn.map { |neighbor| neighbor.doc.name.to_s }.join(",")
+      puts
+      r = NNCB::Recommendation.new(userid)
+      r.similar_users = knn
+      r
     end
   end
+  
+  results.report("vote on repos:") do
+    recommendations.each do |recommendation|
+      p recommendation.ranked_repos(NNCB.users_repos)
+    end
+  end 
+  
 end
