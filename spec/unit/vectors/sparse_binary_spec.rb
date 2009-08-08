40f9733cd0218d5df45118a301dee1275c4753f4
Dan DeLeo <danielsdeleo@mac.com>
2009-08-08 11:30:49 -0600

    An extra check that sparse binary vectors compute distances correctly

--- spec/unit/vectors/sparse_binary_spec.rb
+++ spec/unit/vectors/sparse_binary_spec.rb
@@ -9,4 +9,25 @@ describe Vectors::SparseBinary do
   end
   
   it_should_behave_like "a binary vector"
+  
+  context "getting an extra workout" do
+    
+    before do
+      @token_index_hash = {}
+      1000.times { |i| @token_index_hash[i] = i }
+      @vector_a = @vector_class.new(@token_index_hash)
+      vector_a_items = mock("vector a items")
+      vector_a_items.stub!(:tokens).and_return([5,10,15,20,25,30,35,40,45,50,55,60,65,70,75])
+      @vector_a.convert_document(vector_a_items) # 15 total
+      @vector_b = @vector_class.new(@token_index_hash)
+      vector_b_items = mock("vector b items")
+      vector_b_items.stub!(:tokens).and_return([23,45])
+      @vector_b.convert_document(vector_b_items)
+    end
+  
+    it "should determine the correct distance" do
+      @vector_a.distance(@vector_b).should == 15
+    end
+    
+  end
 end
