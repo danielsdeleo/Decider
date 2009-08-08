674cdd6702fd6d08e69737193cbb1f45b03c39f4
Dan DeLeo <danielsdeleo@mac.com>
2009-08-07 18:24:26 -0600

    Stubs for a KNN based classifier

---
+++ lib/decider/classifiers/nearest_neighbors.rb
@@ -0,0 +1,8 @@
+# encoding: UTF-8
+
+module Decider
+  module Classifiers
+    class NearestNeighbors
+    end
+  end
+end
---
+++ spec/unit/classifiers/nearest_neighbors_spec.rb
@@ -0,0 +1,22 @@
+# encoding: UTF-8
+require File.dirname(__FILE__) + "/../../spec_helper"
+
+describe Classifiers::NearestNeighbors do
+  
+  it "should have a nearest neighbors clusterer" do
+    pending
+  end
+  
+  it "should forward push(class, text) to the clusterer" do
+    pending
+  end
+  
+  it "should define classification name methods on the classifier" do
+    pending
+  end
+  
+  it "should define classification predicate methods on the classifier" do
+    pending
+  end
+  
+end
