17a669749123934117d7ea86e870f59c5c0f3286
Dan DeLeo <danielsdeleo@mac.com>
2009-08-19 00:04:01 -0600

    make_github_recommendations.rb is now for converting repos=>similar repos file into results

--- benchmark/make_github_recommendations.rb
+++ benchmark/make_github_recommendations.rb
@@ -1,11 +1,23 @@
 require File.dirname(__FILE__) + '/bench_helper'
-require File.dirname(__FILE__) + '/github_contest_helper'
+require File.dirname(__FILE__) + '/github_contest'
 
-require "pp"
 
-github_data = GithubContest::DataSet.new(:tanimoto)
-github_data.load_similar_users_from_file
+GithubContest::Data.load!
 
-github_data.recommendations.each do |user_id, repo_ids|
-  puts user_id.to_s + ":" + repo_ids.join(",")
+repos_similar_to = {}
+IO.foreach(BENCH_DIR + "/repos_neighbors.txt") do |line|
+  r = GithubContest::RecommendedRepos.load_string(line.strip)
+  repos_similar_to[r.test_repo] = r
+end
+
+GithubContest::Data.test_users.each do |test_user|
+  recommended_repos = GithubContest::RecommendedRepos.new(test_user)
+  GithubContest::Data.repos_watched_by(test_user).each do |watched_repo|
+    repos_similar_to[watched_repo].repos_with_metrics.each do |similar_repo, metrics|
+      unless GithubContest::Data.repos_watched_by(test_user).include?(similar_repo)
+        recommended_repos.consider_repo(:id=>similar_repo, :metric=>metrics.first)
+      end
+    end
+  end
+  puts recommended_repos.emit_recommendations
 end
